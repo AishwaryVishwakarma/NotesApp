0d37e3856279595ac915363d0abae7d0e1fcfcb6
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-12-27 13:03:22 +0530

    feat: aadded basic profile page and navbar updates

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,98 @@
+import Link from 'next/link';
+import {usePathname, useSearchParams} from 'next/navigation';
 import React from 'react';
 
 import styles from './styles.module.scss';
 
+interface Tab {
+  path: string;
+  name: string;
+  query: Record<string, string | null>;
+  isActive: boolean;
+}
+
 const Navbar = ({className}: {className?: string}) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const userId = searchParams.get('user_id');
+
+  const LEFT_TABS: Tab[] = [
+    {
+      path: '/home',
+      name: 'Home',
+      query: {
+        user_id: userId,
+      },
+      isActive: pathname === '/home',
+    },
+  ];
+
+  const RIGHT_TABS: Tab[] = [
+    {
+      path: '/profile',
+      name: 'Profile',
+      query: {
+        user_id: userId,
+      },
+      isActive: pathname === '/profile',
+    },
+  ];
+
   return (
     <nav className={`${className} ${styles.navbar} `}>
-      <p className={styles.heading}>Notes App</p>
+      <div className={styles.left}>
+        <ul>
+          {LEFT_TABS.map((tab) => {
+            const {path, name, query, isActive} = tab;
+            return (
+              <li key={name} className={styles.tab}>
+                <Link
+                  href={{
+                    pathname: path,
+                    query,
+                  }}
+                  className={isActive ? styles.active : ''}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <p className={styles.heading}>
+        <Link
+          href={{
+            pathname: '/home',
+            query: {
+              user_id: userId,
+            },
+          }}
+        >
+          Notes App
+        </Link>
+      </p>
+      <div className={styles.right}>
+        <ul>
+          {RIGHT_TABS.map((tab) => {
+            const {path, name, query, isActive} = tab;
+            return (
+              <li key={name} className={styles.tab}>
+                <Link
+                  href={{
+                    pathname: path,
+                    query,
+                  }}
+                  className={isActive ? styles.active : ''}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };
